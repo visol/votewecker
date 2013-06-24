4b7ca46f17947a8226fe00a1218bac6a80b9d590
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2013-06-24 21:24:50 +0200

    Initial commit

---
+++ ext_tables.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'easyvote Votewecker');
+
+
+	
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_votewecker_domain_model_voting', 'EXT:votewecker/Resources/Private/Language/locallang_csh_tx_votewecker_domain_model_voting.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_votewecker_domain_model_voting');
+$TCA['tx_votewecker_domain_model_voting'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:votewecker/Resources/Private/Language/locallang_db.xlf:tx_votewecker_domain_model_voting',
+		'label' => 'voting_date',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+
+		'origUid' => 't3_origuid',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'searchFields' => 'voting_date,',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Voting.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_votewecker_domain_model_voting.gif'
+	),
+);
+		
+
+?>
