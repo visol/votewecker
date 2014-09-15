0ae2f57fe36e0bd440758abbd173bd682bcae8a7
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2014-09-15 10:39:12 +0200

    Cleanup and make it compatible to TYPO3 6.2 (refs #358)

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,19 +20,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'shy' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.0',
-			'fluid' => '6.0',
-			'typo3' => '6.0',
+			'typo3' => '6.0.0-6.3.99',
 		),
 		'conflicts' => array(
 		),
