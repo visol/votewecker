8e31a47d270c9a16e5aff098e3616403e9928a4a
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-03-17 15:28:09 +0000

    [TASK] Add composer.json, set TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.3.99',
+			'typo3' => '6.0.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
